64282c71f8c15836e5f9c93d3fc90336e0b209c9
white-sweatshirt <wawer.franciszek@gmail.com>
2025-05-29 23:04:06 +0200

    Add files via upload

--- ONP-calculator-getline adaptation/main.c
+++ ONP-calculator-getline adaptation/main.c
@@ -8,93 +8,84 @@
 #define NUMBER '0' /*sygnal znaleznia liczby*/
 #define MAXVAL 100 /* MAX dlugosc stotsu*/
 
-#define SHOW_STACK '~'
+#define SHOW_STACK '='
 #define CLEAN_STACK '!'
-#define FINISHED_LINE -2
-#define COS 0
-#define SIN 1
-#define POWER 2
-#define EXP 3
 
-#define MAXVARIBLES 'z' - 'a'
-#define VARIBLE_OPTION 'v'
+#define MAXVARIBLES 'z' - 'a' + 1
 
 int getline(char s[], int limit);
 double pop(void);
 void push(double a);
+
 void showStack(void);
 void clearStack(void);
 
-int mathExpr(char s[], int i, double varibles[]);
+void giveBoundriesOfFragment(char s[], int *start, int *finish);
+int sayWhatTypeItIs(char s[], int start);
 
+void copyFragmentToNewTable(char s[], char s1[], int start, int finish);
 int main()
 {
 
-    int type = 0;
+    char type = 0;
     double op2 = 0.0;
-    char s[MAXOP];
-    double varibles[MAXVARIBLES]; // everything w petli wiekszej wczytujacyej wieksze liczby
-    while ((type) != EOF)         // tu bedzie funkcja dajaca typ dla danej lonie
+    char s[MAXOP], buffor[MAXOP];
+
+    int endingIndex = MAXOP;
+    int beggingIndex = 0;
+    do // petla po koljenych liniach
     {
-        switch (type)
-        {
-        case NUMBER:
-            // dodaj wczesniej konwersje na numerek
-            push(atof(s));
-            // pamietaj o usunieciu tablicy buforowej
-            break;
-        case '+':
-            push(pop() + pop());
-            break;
-        case '-':
-            op2 = pop();
-            push(pop() - op2);
-            break;
-        case '*':
-            push(pop() * pop());
-            break;
-        case '/':
-            op2 = pop();
-            push(pop() / op2);
-            break;
-        case '%': // thow away ulamki
-            int iop2 = (int)pop();
-            push((int)pop() % iop2);
-            break;
-        case SHOW_STACK:
-            showStack();
-            break;
-        case '\n':
-            printf("\t %.8f\n", pop());
-            break;
-        case EXP:
-            op2 = exp(pop());
-            push(op2);
-            break;
-        case SIN:
-            push(sin(pop()));
-            break;
-        case COS:
-            push(cos(pop()));
-            break;
-        case POWER:
-            op2 = pop();
-            push(pow(pop(), op2));
-            break;
-        case VARIBLE_OPTION:
-            break;
-        case CLEAN_STACK:
-            clearStack();
-            break;
-        default:
-            printf("blad nie znane polecnie !");
-            break;
-        }
-    }
-    for (int i = 0; i < MAXVARIBLES; i++)
-        printf("\n %c=%.5f", 'a' + i, varibles[i]);
-    ungets(s);
-    showBufor();
+        getline(s, MAXOP);
+        endingIndex = 0;
+        beggingIndex = 0;
+        bool wasThereIteration = false;
+        do
+        { // petla po kolejnych czesciach lini
+            beggingIndex = endingIndex;
+            wasThereIteration = true;
+            giveBoundriesOfFragment(s, &beggingIndex, &endingIndex);
+
+            type = sayWhatTypeItIs(s, beggingIndex);
+            if (beggingIndex >= endingIndex)
+                break;
+
+            switch (type)
+            {
+            case NUMBER:
+                copyFragmentToNewTable(s, buffor, beggingIndex, endingIndex);
+                push(atof(buffor));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '/':
+                op2 = pop();
+                push(pop() / op2);
+                break;
+            case '%': // thow away ulamki
+                int iop2 = (int)pop();
+                push((int)pop() % iop2);
+                break;
+            case SHOW_STACK:
+                showStack();
+                break;
+            case CLEAN_STACK:
+                clearStack();
+                break;
+            default:
+                printf("blad nie znane polecnie !");
+                break;
+            }
+        } while (s[beggingIndex] != EOF && s[beggingIndex] != '\n');
+    } while ((type) != EOF);
+
     return 0;
 }
 // Text function
@@ -107,50 +98,46 @@ int getline(char s[], int limit)
     s[i] = '\0';
     return i;
 }
-int mstrlen(char s[])
-{
-    if (s[0] == '\0')
-        return 0;
-    int i = 0;
-    while (s[i++] != '\0')
-        ;
-    return i + 1;
-}
+
 void copyFragmentToNewTable(char s[], char s1[], int start, int finish)
 {
     int i = 0;
     while (start < finish)
-        *(s1 + i) = *(s + start++);
-}
-int *declereTable(int size)
-{
-    int *t = malloc(size * sizeof(int));
-    return t;
-}
+        *(s1 + i++) = *(s + start++);
+    *(s1 + i) = '\0';
+} // We can never dig too deep
+
 int giveBeggingIndex(char s[], int start)
 {
-    if (!(*(s + start)))
-        return FINISHED_LINE;
-    while (isspace(*(s + start++)))
-        ;
+    if (*(s + start) == '\0')
+        return start;
+    while (isspace(*(s + start)))
+        start++;
     return start;
 } // i.e getting rid of whitespaces
 
 int giveEndingIndex(char s[], int start)
 {
-    if (start < 0)
-        return start;
-    if (isdigit(*(s + start)))
+    if (*(s + start) == '\0')
+        return start; // here is a case as if it was null symbol
+    else if (isdigit(*(s + start)))
     {
-        while (isdigit(*(s + start++)))
-            ;
+        while (isdigit(*(s + start)))
+            start++;
+
+        if (*(s + start) == '.')
+        {
+            start++;
+            while (isdigit(*(s + start)))
+                start++;
+        }// fractions support
         return start;
     }
     else
-        return start + 1; // otherwise it is symbol and takes one room
-}
+        return start + 1; // otherwise it is symbol and takes one space
+} // returun 1 grater symbol than you need
 
-void giveSizeOfFragment(char s[], int *start, int *finish)
+void giveBoundriesOfFragment(char s[], int *start, int *finish)
 {
     *start = giveBeggingIndex(s, *start);
     *finish = giveEndingIndex(s, *start);
@@ -158,8 +145,13 @@ void giveSizeOfFragment(char s[], int *start, int *finish)
 
 int sayWhatTypeItIs(char s[], int start)
 {
+    if ('0' <= s[start] && s[start] <= '9')
+        return NUMBER;
+    else
+        return *(s + start); // otherwise it is binary symbol
 }
 // Text function -END
+// Stack -START
 int stackP = 0;
 double valStack[MAXVAL]; // Tu sa wartosc przechowywane i.e tu jest stos
 
@@ -181,72 +173,14 @@ double pop(void)
         return 0.0;
     }
 }
-
+#define ADJUSTMENT_FOR_LAST 1
 void showStack()
 {
-    for (int i = stackP; 0 <= i; i++)
-        printf("%.2f ", valStack[i]);
+    for (int i = stackP - ADJUSTMENT_FOR_LAST; 0 <= i; i--)
+        printf("%.2lf \n", valStack[i]);
 }
-
 void clearStack()
 {
     stackP = 0;
 }
-
-#define WORDS 5
-#define NMATH_FUNCTIONS 4
-#define FIRST_LETTER 0
-
-void initVarible(double varibles[], int c)
-{
-    double temp = pop();
-    varibles[c - 'a'] = temp;
-    push(temp);
-} // simplest implementaion of coping varible
-
-bool isVarilbe(char s[], int c, int i, int primeI, double varibles[])
-{
-
-    if (primeI == (i - 1) && isspace(s[i]))
-    {
-        initVarible(varibles, s[i - 1]);
-        ungetch(c);
-        return 1;
-    }
-    else
-        return 0;
-}
-
-int giveFunctionNumber(char s[], int c, int i, double varibles[])
-{
-
-    s[i] = '\0';
-    char functions[NMATH_FUNCTIONS][WORDS] = {{"cos"}, {"sin"}, {"power"}, {"exp"}}; // odpowiednio 0,1,2,3
-    for (int j = 0; j < NMATH_FUNCTIONS; j++)
-    {
-        int isTheSameWord = 1;
-        for (int m = 0; m < WORDS; m++)
-        {
-            if (s[m] != functions[j][m])
-            {
-                isTheSameWord = 0;
-                break;
-            }
-        }
-        if (isTheSameWord > 0)
-            return j;
-    }
-    return -2;
-} // TO DO : Rewrite using given string and pointers in it
-int mathExpr(char s[], int i, double varibles[])
-{
-    int primeI = i;
-    int c = s[i];
-    while (islower(s[++i] = c = getch()))
-        ;
-    if (isVarilbe(s, c, i, primeI, varibles)) // jezeli nie zmienna to moze funkcja ?
-        return 'v';
-    else
-        return giveFunctionNumber(s, c, i, varibles);
-} // function handling matematical expressions
-// TO DO : Rewrite using given string and pointers in it
+// Stack Ends here
